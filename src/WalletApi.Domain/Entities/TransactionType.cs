b34718c9666f2b65429b080d85835b4608ee1d1c
JohanCante <joescava@gmail.com>
2025-04-02 00:30:20 -0500

    feat(domain): modelos Wallet y TransactionHistory definidos con sus relaciones y enum

---
+++ src/WalletApi.Domain/Entities/TransactionType.cs
@@ -0,0 +1,7 @@
+namespace WalletApi.Domain.Entities;
+
+public enum TransactionType
+{
+    Credit = 1,
+    Debit = 2
+}
---
+++ src/WalletApi.Domain/Entities/Wallet.cs
@@ -0,0 +1,10 @@
+namespace WalletApi.Domain.Entities;
+
+public class WalletApi{
+    public int id {get; set; }
+    public string DocumentId {get; set; } = default;
+    public string Name {get; set; } = default;
+    public decimal Balance {get; set; }
+    public DateTime CreatedAt {get; set; }
+    public DateTime UpdateAt {get; set; }
+}
