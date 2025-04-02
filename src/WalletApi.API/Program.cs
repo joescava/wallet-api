fc56e857b7091f1ea6831afdda93d7b8f3fb8ff4
JohanCante <joescava@gmail.com>
2025-04-01 23:04:16 -0500

    feat: configurar DependencyInjection para EF Core y repositorios

--- src/WalletApi.API/Program.cs
+++ src/WalletApi.API/Program.cs
@@ -1,9 +1,11 @@
+using WalletApi.Infrastructure.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
